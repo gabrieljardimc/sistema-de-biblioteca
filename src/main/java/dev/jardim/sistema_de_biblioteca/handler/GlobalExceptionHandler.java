58aa22784e7c2c601d06a3964b4e57e40bbe4f3e
Gabriel Jardim <103118445+gabrieljardimc@users.noreply.github.com>
2026-05-25 16:18:01 -0300

    Update GlobalExceptionHandler.java

--- src/main/java/dev/jardim/sistema_de_biblioteca/handler/GlobalExceptionHandler.java
+++ src/main/java/dev/jardim/sistema_de_biblioteca/handler/GlobalExceptionHandler.java
@@ -25,7 +25,7 @@ public class GlobalExceptionHandler {
                 .message(exception.getMessage())
                 .status(HttpStatus.BAD_REQUEST.value())
                 .build();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
     @ExceptionHandler(Exception.class)
